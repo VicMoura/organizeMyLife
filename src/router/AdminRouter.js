4306434b95a96ca5850189e8cf95bd1d0452f67f
VicMoura <vicmourator@gmail.com>
2023-03-09 17:37:51 -0300

    feature(tarefas): Finalizado a criação da página de tarefas(#11)

--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -3,6 +3,10 @@ const router  = express.Router();
 const adminAuth = require("../middleware/adminAuth");
 const User = require("../model/User");
 const Tarefa = require("../model/Tarefa");
+const Item = require("../model/Item");
+
+let itens; 
+
 
 router.get('/tarefas', adminAuth,  (req, res) => {
     let id = req.session.user.id; 
@@ -19,6 +23,31 @@ router.get('/tarefas', adminAuth,  (req, res) => {
 
 router.get('/user', adminAuth, (req, res) => {
     res.render('templates/admin/user', {user : req.session.user});
+});
+
+router.get('/viewTarefas', adminAuth, (req, res) => {
+    let id = req.session.user.id;
+    let idTarefa = req.params.idTarefa;
+    console.log('vendo o ID', idTarefa);
+    Tarefa.findAll({
+        where : {
+            userId : id,
+            id : idTarefa
+        }
+    }).then(
+        tarefas => {
+            Item.findAll({
+                where : {
+                    tarefaId : idTarefa
+                }
+            }).then((item) => {
+                res.render('templates/admin/viewTarefa', {tarefa : tarefas, item : item});
+            }).catch((erro) => {
+                console.log(erro); 
+                res.redirect('/tarefas');
+            });
+        });
+
 });
 
 router.post('/apagarConta', adminAuth, (req, res) => {
@@ -55,7 +84,7 @@ router.post('/criarLista', adminAuth, (req, res) => {
         nome : nome, 
         situacao : situacao,
         userId : id
-    }).then(() => {
+    }).then((tarefa) => { 
         res.redirect("/tarefas"); 
     }).catch((erro) => {
         console.log(erro);
@@ -65,21 +94,102 @@ router.post('/criarLista', adminAuth, (req, res) => {
 
 router.post('/viewTarefa', adminAuth, (req, res) => {
     let id = req.body.tarefaId; 
-
-
+    req.session.tarefa = {
+        id : id
+    }
      Tarefa.findOne({
         where : {
             id : id
         }
     }).then(
         tarefas => {
-            res.render('templates/admin/viewTarefa', {tarefas : tarefas});
-        }
-    )
+            Item.findAll({
+            }).then((item) => {
+                res.render('templates/admin/viewTarefa', {tarefa : tarefas, item : item});
+            }).catch((erro) => {
+                console.log(erro); 
+                res.redirect('/tarefas');
+            })
+           
+    }).catch((erro) => {
+        console.log(erro); 
+        res.redirect('/tarefas');
+    })
 
   
 });
 
 
+router.post('/criarItem', adminAuth, (req, res) => {
+
+    let nome = req.body.nome; 
+    let check = req.body.check; 
+    let id = req.body.tarefaId
+    Tarefa.findOne({
+        where : {
+            id : id
+        }
+    }).then(
+        tarefas => {
+            Item.create({
+                nome : nome, 
+                check : check,
+                tarefaId : id
+            }).then(() => {
+                    Item.findAll({
+                    }).then((item) => {
+                        res.render('templates/admin/viewTarefa', {tarefa : tarefas, item : item});
+                    }).catch((erro) => {
+                        console.log(erro); 
+                        res.redirect('/tarefas');
+                    })
+            }).catch((erro) => {
+                console.log(erro);
+                res.redirect('/tarefas'); 
+            })
+    }).catch((erro) => {
+        console.log(erro); 
+        res.redirect('/tarefas');
+    })
+
+          
+
+});
+
+router.post('/deleteItem', adminAuth, (req, res) => {
+
+    let item = req.body.checkbox;
+    let id = req.body.idTarefa; 
+    console.log('SOCORROOO', id);
+
+
+    Tarefa.findOne({
+        where : {
+            id : id
+        }
+    }).then(
+        tarefas => {
+                Item.destroy({where: {
+                    id : item
+                }}).then(function () {
+                        Item.findAll({}).then((itens) => {
+                                res.render('templates/admin/viewTarefa', {tarefa : tarefas, item : itens});
+                        }).catch((erro) => {
+                            console.log(erro); 
+                            res.redirect('/tarefas');
+                        })
+                }).catch((erro) => {
+                        console.log(erro); 
+                        res.redirect('/tarefas');
+                });
+    }).catch((erro) => {
+        console.log(erro); 
+        res.redirect('/tarefas');
+    })
+
+    
+    
+
+});
 
 module.exports = router; 
