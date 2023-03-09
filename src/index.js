69fa27cc16ad64d7079e4adb7aa986170a0d79ff
VicMoura <vicmourator@gmail.com>
2023-03-08 21:43:38 -0300

    feature(login) : Criando a página de informacao de usuario e permitindo deleter(#13)

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ app.use(session({
     }
 }));
 
+
 //Database
 connection.authenticate()
 .then(()=>
--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -1,9 +1,36 @@
 const express = require("express"); 
 const router  = express.Router(); 
 const adminAuth = require("../middleware/adminAuth");
+const User = require("../model/User");
 
 router.get('/tarefas', adminAuth,  (req, res) => {
     res.render('templates/admin/tarefas');
 }); 
+
+router.get('/user', adminAuth, (req, res) => {
+    res.render('templates/admin/user', {user : req.session.user});
+});
+
+router.post('/apagarConta', adminAuth, (req, res) => {
+    let id = req.body.id; 
+    if(id != undefined){
+        if(!isNaN(id)){
+            User.destroy({
+                where : {
+                    id : id
+                }
+            }).then(() => {
+                res.redirect("/"); 
+            }).catch((erro) => {
+                console.log(erro); 
+                res.redirect("/user"); 
+            }); 
+
+        }else{
+            res.redirect("/user"); 
+        }
+    }
+});
+
 
 module.exports = router; 
